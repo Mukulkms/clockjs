630d3d4728b8c12421e58deb7bbc84f1cf21a87e
mukulkms <mukulkms.iimtgn@gmail.com>
2022-10-24 15:25:29 +0530

    .

--- index.js
+++
@@ -1,4 +0,0 @@
-document.body.onmousemove = function(e) {
-    document.documentElement.style.setProperty('--x',(e.clientX)+'px');
-    document.documentElement.style.setProperty('--y',(e.clientY)+'px');
-  }
